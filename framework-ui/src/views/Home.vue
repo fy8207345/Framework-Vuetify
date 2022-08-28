418ea09694b7553347e4f710bd509bf526957536
fengyan <fy@zwyun.cc>
2022-08-28 12:14:43 +0800

    material资源

---
+++ framework-ui/src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app>
+    <v-app-bar
+        app
+        color="primary"
+        dark
+    >
+      <div class="d-flex align-center">
+        系统名称
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+          href="https://github.com/vuetifyjs/vuetify/releases/latest"
+          target="_blank"
+          text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <loginPage />
+    </v-main>
+
+    <v-footer>
+      底部
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ framework-ui/src/views/loginPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex h-100 w-100">
+    <div class="h-100 w-100 flex bg-primary">
+      1
+    </div>
+    <div class="h-100 w-100 flex">
+      2
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "loginPage"
+}
+</script>
+
+<style scoped>
+
+</style>
