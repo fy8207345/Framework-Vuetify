c22a3f4e09784a80fb7cd87727e08fd661ad811c
fengyan <fy@zwyun.cc>
2022-08-28 19:21:45 +0800

    页面

--- framework-ui-ts/src/views/LoginView.vue
+++ framework-ui-ts/src/views/LoginView.vue
@@ -1,24 +1,69 @@
 <template>
-  <div class="flex h-100 w-100">
-    <div class="h-100 w-100 flex bg-primary">
-      <div class="margin app-name">
+  <div class="d-lg-flex" style="flex: 1 2">
+    <v-container class="bg-primary flex-column padding">
+      <label class=" app-name">
         系统名称
-      </div>
-    </div>
-    <div class="h-100 w-100 flex">
-      2
-    </div>
+      </label>
+      <v-row no-gutters>
+        <v-col :cols="24" :lg="24">
+          <label class="margin-top app-name">
+            登录
+          </label>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-container class="flex-column padding">
+      <label class=" app-name" >登录</label>
+      <v-form class="margin-top">
+        <v-text-field
+          rounded
+          validate-on-blur
+          clearable
+          counter
+          outlined
+          :height="36"
+          label="输入用户名"
+          placeholder="输入用户名"/>
+
+        <v-text-field
+          v-model="password"
+          rounded
+          validate-on-blur
+          counter
+          outlined
+          :append-icon="showPassowrd ? 'mdi-eye' : 'mdi-eye-off'"
+          :type="showPassowrd ? 'text' : 'password'"
+          @click:append="showPassowrd = !showPassowrd"
+          :height="36"
+          label="输入密码"
+          placeholder="输入密码"/>
+
+        <v-btn type="submit" />
+      </v-form>
+    </v-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: "LoginView"
+  name: "LoginView",
+  data(){
+    return {
+      showPassowrd: false,
+      password: null
+    }
+  }
 }
 </script>
 
 <style scoped>
 .margin{
   margin: 90px 48px;
+}
+.margin-top{
+  margin-top: 48px;
+}
+.padding{
+  padding: 90px 48px;
 }
 </style>
